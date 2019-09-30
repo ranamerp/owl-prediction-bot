aff0ad0600ebc77e7002f239474bf8907fd882ae
Prem Rana <ranamerp@gmail.com>
2019-09-29 23:18:02 -0500

    Collected map data Collected data of roster per map Added what stage map was played in Added more constants to constants.py

--- api.py
+++ api.py
@@ -2,28 +2,28 @@ import requests
 import json
 
 import pandas as pd
+import logging, sys
 import os
 
 # Local Imports
-from constants import MAP_TYPE
+from constants import MAP_TYPE, ID_TO_NAME, MATCH_COLUMS
 
 #API Requests
 schedule = requests.get("https://api.overwatchleague.com/schedule")
 schedule_2018 = requests.get("https://api.overwatchleague.com/schedule?season=2018")
 
-#Dictionaries (May move to constants)
-team_id = [7698, 4402, 7692, 4523, 4407, 7699, 7693, 4525, 4410, 4406, 4405, 4403, 7694, 4524, 4404, 4409, 4408, 7695, 7696, 7697]
-DF_COLUMNS = ["date", "away", "away score", "home", "home score","winner"]
-MATCH_COLUMS = ["Map 1 Name", "Map 1 Type", "Map 1 Away Points", "Map 1 Home Points",
-                "Map 2 Name", "Map 2 Type", "Map 2 Away Points", "Map 2 Home Points",
-                "Map 3 Name", "Map 3 Type", "Map 3 Away Points", "Map 3 Home Points",
-                "Map 4 Name", "Map 4 Type", "Map 4 Away Points", "Map 4 Home Points",
-                "Map 5 Name", "Map 5 Type", "Map 5 Away Points", "Map 5 Home Points"]
+#Dictionaries
+DF_COLUMNS = ["date", "stage", "away", "away score", "home", "home score","winner"]
+match_colums = MATCH_COLUMS
+MAP_COLUMNS = ["Name", "Type", "Away Points", "Home Points"]
 
 #JSON Loading of API Repsonses
 sd = json.loads(schedule.text)
 sd18 = json.loads(schedule_2018.text)
 
+#Other Initializations
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.debug("Debug Activated")
 
 
 #gets the entire OWL Schedule
@@ -32,9 +32,11 @@ def get_schedule(schedule, df):
         if i["name"] == "All-Stars":
             continue
         for j in i["matches"]:
-            if j['conclusionStrategy'] == "BEST_OF":
+            if j['conclusionStrategy'] == "BEST_OF" or j['conclusionStrategy'] == "FIRST_TO":
                 continue
+            id = j["id"]
             date = j["startDate"]
+            stage = j["bracket"]["stage"]["tournament"]["title"]
             away = j["competitors"][0]["name"]
             away_score = j["scores"][0]["value"]
             home = j["competitors"][1]["name"]
@@ -44,72 +46,71 @@ def get_schedule(schedule, df):
             except KeyError:
                 winner = "N/A"
                 continue
-            match_series = get_match_data(j)
-            dl = pd.Series([date, away, away_score, home, home_score, winner], DF_COLUMNS)
-            d2 = pd.concat([dl, match_series])
-            df = df.append(d2, ignore_index=True)
+            match_series = get_match_data(id, away, home)
+            dl = pd.Series([date, stage, away, away_score, home, home_score, winner], DF_COLUMNS)
+            for k in match_series:
+                dl = pd.concat([dl, k])
+            logging.debug("%s vs %s recorded!", str(away), str(home))
+            df = df.append(dl, ignore_index=True)
+
+
 
     return df
-def get_match_data(m):
-    try:
-        map_1_name = m["games"][0]["attributes"]["map"]
-    except KeyError:
-        map_1_name = "None"
-    map_1_type = MAP_TYPE.get(map_1_name)
-    map_1_away_points = m["games"][0]["points"][0]
-    map_1_home_points = m["games"][0]["points"][1]
 
-    try:
-        map_2_name = m["games"][1]["attributes"]["map"]
-    except KeyError:
-        map_2_name = "None"
-    map_2_type = MAP_TYPE.get(map_2_name)
-    map_2_away_points = m["games"][1]["points"][0]
-    map_2_home_points = m["games"][1]["points"][1]
+def get_match_data(id, away, home):
+    matches = requests.get("https://api.overwatchleague.com/matches/{}".format(id))
+    m = json.loads(matches.text)
+    match_series = []
+    for i in m["games"]:
+        number = i["number"]
+        try:
+            name = i["attributes"]["map"]
+            type = MAP_TYPE.get(name)
+        except KeyError:
+            name = None
+            type = None
+        try:
+            away_points = i["points"][0]
+            home_points = i["points"][1]
+        except:
+            away_points = None
+            home_points = None
+        colums = ["Map " + str(number) + " " + j for j in MAP_COLUMNS]
+        map_numbers = pd.Series([name, type, away_points, home_points], colums)
+        roster_info = get_match_player(i, away, home)
+        map_data = pd.concat([map_numbers, roster_info[0], roster_info[1]])
+        match_series.append(map_data)
+    return match_series
 
-    try:
-        map_3_name = m["games"][2]["attributes"]["map"]
-    except KeyError:
-        map_3_name = "None"
-    map_3_type = MAP_TYPE.get(map_3_name)
-    map_3_away_points = m["games"][2]["points"][0]
-    map_3_home_points = m["games"][2]["points"][1]
-
-    try:
-        map_4_name = m["games"][3]["attributes"]["map"]
-    except KeyError:
-        map_4_name = "None"
-    map_4_type = MAP_TYPE.get(map_4_name)
-    map_4_away_points = m["games"][3]["points"][0]
-    map_4_home_points = m["games"][3]["points"][1]
-
-    try:
-        map_5_name = m["games"][4]["attributes"]["map"]
-        map_5_type = MAP_TYPE.get(map_5_name)
-        map_5_away_points = m["games"][4]["points"][0]
-        map_5_home_points = m["games"][4]["points"][1]
-    except (IndexError, KeyError) as e:
-        map_5_name = "N/A"
-        map_5_type = "N/A"
-        map_5_away_points = "N/A"
-        map_5_home_points = "N/A"
-    series = pd.Series([map_1_name, map_1_type, map_1_away_points, map_1_home_points,
-                        map_2_name, map_2_type, map_2_away_points, map_2_home_points,
-                        map_3_name, map_3_type, map_3_away_points, map_3_home_points,
-                        map_4_name, map_4_type, map_4_away_points, map_4_home_points,
-                        map_5_name, map_5_type, map_5_away_points, map_5_home_points], MATCH_COLUMS)
-    return series
-
-
+def get_match_player(m,away,home):
+    away_team_colums = ["Away Player 1", "Away Player 2", "Away Player 3", "Away Player 4", "Away Player 5", "Away Player 6"]
+    home_team_colums = ["Home Player 1", "Home Player 2", "Home Player 3", "Home Player 4", "Home Player 5", "Home Player 6"]
+    home_team = []
+    away_team = []
+    for i in m["players"]:
+        player_name = i['player']['name']
+        team_id = i["team"]["id"]
+        player_team = ID_TO_NAME.get(team_id)
+        if player_team == away:
+            away_team.append(player_name)
+        elif player_team == home:
+            home_team.append(player_name)
+    ht_colums = ["Map " + str(m['number']) + " " + j for j in home_team_colums]
+    at_colums = ["Map " + str(m['number']) + " " + j for j in away_team_colums]
+    ht= pd.Series(home_team, ht_colums)
+    at= pd.Series(away_team, at_colums)
+    return at, ht
 
 #gets data from api for each team and adds it to a list of dataframes
 def get_data(df):
     dataframes = []
     data2018 = get_schedule(sd18["data"]["stages"], df)
     dataframes.append(data2018)
+    print("The 2018 Season was successfully collected")
     data2019 = get_schedule(sd["data"]["stages"], df)
     dataframes.append(data2019)
-    dataframes_con = pd.concat(dataframes)
+    print("The 2019 Season was successfully collected")
+    dataframes_con = pd.concat(dataframes, sort=False)
 
     return dataframes_con
 
@@ -117,7 +118,8 @@ def get_data(df):
 if __name__ == "__main__":
 
     # create an empty dataframe
-    owl_df = pd.DataFrame(columns=DF_COLUMNS)
+    owl_df = pd.DataFrame(columns=DF_COLUMNS + match_colums)
+
 
     #fill data
     owl_df = get_data(owl_df)
@@ -129,7 +131,7 @@ if __name__ == "__main__":
 
 
     #write to csv
-    owl_df.to_csv('data.csv', header=DF_COLUMNS + MATCH_COLUMS, index=False)
+    owl_df.to_csv('data.csv', header=DF_COLUMNS + match_colums, index=False)
 
 
 
